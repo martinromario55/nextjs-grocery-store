276f8a8ace921cd4da9c435c3c4456db976ad733
martinromario55 <martinromario55@gmail.com>
2024-05-24 19:53:18 +0300

    Cart

---
+++ app/_components/AddToCart.jsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const AddToCart = () => {
+  return (
+    <div>
+      <h1>Cart</h1>
+      <Button>Add To Cart</Button>
+    </div>
+  )
+}
+
+export default AddToCart
